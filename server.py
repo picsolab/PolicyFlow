f66084981d6bc79de0d2f446240cc22854d6b507
ayong8 <yong8@me.com>
2018-02-14 16:00:22 -0500

    Changed server.py port

--- server.py
+++ server.py
@@ -5,5 +5,5 @@ from app.services.web_service import NetworkService
 
 if __name__ == '__main__':
     NetworkService.get_specified_diffusion2_by('d')
-    port = int(os.environ.get('PORT', 50005))
+    port = int(os.environ.get('PORT', 9001))
     app.run(host='0.0.0.0', port=port, debug=True)
