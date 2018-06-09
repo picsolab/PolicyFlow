cfa3eaa63ecfd6f983d0d675edc3b4f5b215c8fb
ayong8 <yong8@me.com>
2018-06-08 23:12:41 -0400

    changed the year at the bottom

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ from app import app
 from app.services.web_service import NetworkService
 
 if __name__ == '__main__':
-    NetworkService.get_specified_diffusion2_by('d')
     port = int(os.environ.get('PORT', 9001))
-    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=port, threaded=True)
+    
